a9ce15a0c210c302f7d50a4cd5cfd038090dfebd
Goga Eva <igorevtushenko22@gmail.com>
2021-06-08 04:48:25 +0300

    .

---
+++ lib/algorithms/fermat_factorization.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:math';
+
+Map<String, dynamic> fermatFactorization(dynamic inputValue) {
+  Map<String, dynamic> result = Map();
+  final stopwatch = Stopwatch()..start();
+  result['iterations'] = 0;
+  if (inputValue.isEmpty) {
+    result['value'] = 'Input value must be an integer';
+    return result;
+  }
+  final inputNumber = int.parse(inputValue);
+  if ((inputNumber & 1) == 0) {
+    result['iterations']++;
+    result['value'] = '${inputNumber / 2} * 2';
+    return result;
+  }
+  var a = sqrt(inputNumber).ceil();
+  if (a * a == inputNumber) {
+    result['iterations']++;
+    result['value'] = '$a * $a';
+    return result;
+  }
+  var b;
+  sleep(Duration(milliseconds: 995));
+  while (true) {
+    int b1 = a * a - inputNumber;
+    b = sqrt(b1).ceil();
+    if (b * b == b1)
+      break;
+    else
+      a += 1;
+    result['iterations']++;
+  }
+  print(stopwatch.elapsedMilliseconds);
+  if (stopwatch.elapsedMilliseconds > 1000) {
+    result['value'] = 'Time is up(1 sec)';
+  } else {
+    result['value'] = '${a - b} * ${a + b}';
+  }
+  return result;
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:rts_mobile/screens/fermat_factorization_screen.dart';
+import 'package:rts_mobile/screens/genetic_Diophantus_screen.dart';
+import 'package:rts_mobile/screens/perceptron_screen.dart';
+import 'package:rts_mobile/widgets/bottom_navigation.dart';
+
+List<Widget> _options = <Widget>[
+  FermatFactorization(),
+  Perceptron(),
+  GeneticAlgorithm(),
+];
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(allTabItems[_currentIndex].title),
+        ),
+      ),
+      body: _options.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigation(
+        currentIndex: _currentIndex,
+        onSelectTab: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
